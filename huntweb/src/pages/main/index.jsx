045be1c7f666770c6688aaf374b4cc82119e6c2a
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-06-21 15:34:56 -0300

    Prev and Next button prev

--- huntweb/src/pages/main/index.jsx
+++ huntweb/src/pages/main/index.jsx
@@ -47,7 +47,7 @@ export default class Main extends Component{
   }
 
   render(){
-    const {products} = this.state;
+    const {products, page, productInfo} = this.state;
 
     return(
       <div className='product-list'>
@@ -58,8 +58,8 @@ export default class Main extends Component{
             <a href="#">Acessar</a>
           </article>)}
           <div className='actions'>
-            <button onClick={this.prevPage}>Anterior</button>
-            <button onClick={this.nextPage}>Próximo</button>
+            <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+            <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
           </div>
       </div>
     )
